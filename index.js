60405c2a2293eef7b184a507543236cc9b873230
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-06-24 11:13:09 +0200

    Use authentication to increase rate limit of GitHub

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict';
 
 var Hapi = require('hapi');
+var Fs = require('fs');
+var Yaml = require('js-yaml');
 var GitHubApi = require('github');
 
 var Github = new GitHubApi({
     version: "3.0.0",
     timeout: 5000
 });
+
+//load config and authenticate if there is a config
+if (Fs.existsSync('config.yml')) {
+    var app = Yaml.safeLoad(Fs.readFileSync('config.yml', 'utf8'));
+    Github.authenticate({
+        type: "basic",
+        username: app.github.user,
+        password: app.github.password
+    });
+}
 
 // Create a server with a host and port
 var server = Hapi.createServer('0.0.0.0', 8080);
